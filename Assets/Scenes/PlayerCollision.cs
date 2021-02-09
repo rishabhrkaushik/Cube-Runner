cd3cc912340397c941a1c21efb45f3c5db319c54
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2021-02-09 18:00:42 +0530

    coobstacle and collision

---
+++ Assets/Scenes/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        // Debug.Log(collisionInfo.collider.name);
+        // Debug.Log(collisionInfo.collider.tag);
+
+        if(collisionInfo.collider.tag == "obstacle")
+        {
+            Debug.Log("You hit Obstacle");
+        }
+    }
+}
