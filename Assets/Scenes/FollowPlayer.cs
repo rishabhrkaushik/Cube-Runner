ccbe25eae8af02c38da91eb5a2b55dd789266950
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2021-02-09 17:07:03 +0530

    camera follow player position

---
+++ Assets/Scenes/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(0, 1, -5);
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
