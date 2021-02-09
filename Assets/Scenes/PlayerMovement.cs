6a68936e0ae83d97ff9460aa73ed282caa465bb0
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2021-02-09 16:57:50 +0530

    player movement on keyboard input

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -3,17 +3,28 @@
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public float zForce = 500;
+    public float xForce = 500;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Player Movement Start");
         // rb.useGravity = false; // Turn off gravity
+
     }
 
     // Update is called once per frame
     void FixedUpdate() // use fixed update when using physics
     {
-        rb.AddForce(0, 0, 500 * Time.deltaTime); // Time.deltaTime to compensate for frame rate
+        rb.AddForce(0, 0, zForce * Time.deltaTime); // Time.deltaTime to compensate for frame rate
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(xForce * Time.deltaTime, 0, 0);
+        }
+        else if(Input.GetKey("a"))
+        {
+            rb.AddForce(-xForce * Time.deltaTime, 0, 0);
+        }
     }
 }
