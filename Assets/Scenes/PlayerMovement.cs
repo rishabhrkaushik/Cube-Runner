fd93d6d17f447dbc715d478534b32ed3e9e42c15
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2021-02-09 13:41:08 +0530

    add force to player

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Player Movement Start");
+        rb.useGravity = false; // Turn off gravity
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() // use fixed update when using physics
+    {
+        rb.AddForce(0, 0, 500 * Time.deltaTime); // Time.deltaTime to compensate for frame rate
+    }
+}
