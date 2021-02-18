01bd2f3dc1c4213570eac726053b33ee6bf56278
Rishabh Kaushik <rishabhrkaushik@gmail.com>
2021-02-18 11:32:38 +0530

    Welcome scene and enter game

---
+++ Assets/Scenes/Scripts/Welcome.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Welcome : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        Debug.Log("Loading First Level");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
